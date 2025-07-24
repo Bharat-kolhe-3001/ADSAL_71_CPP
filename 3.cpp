be88b570e88f6152cc8e2a89dfa1243972ce442d
Bharat kolhe <163564571+Iambharatkolhe@users.noreply.github.com>
2025-07-24 10:00:17 +0530

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter how many trailing zeros are required: ";
+    cin >> n;
+
+    int N = (n * 5) - 1;
+    int result;
+    
+
+    for (int i = 1; i <= N; i++) {
+
+        result = 1;
+
+        for (int j = 1; j <= i; j++) {
+            result = result * j;
+        }
+
+        int temp = result;
+        int count = 0;
+
+        while (temp % 10 == 0 ) {
+            count++;
+            temp = temp / 10;
+        }
+
+        if (count == n || count == n-1) {
+            cout << "The first number whose factorial has " << n << " trailing zeros is: " << i << endl;
+          
+            break;
+        }
+    }
+    return 0;
+}
