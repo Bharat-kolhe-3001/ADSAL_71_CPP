5c8518435977d897df5b17b91a5e6fe33f7dd716
Bharat kolhe <163564571+Iambharatkolhe@users.noreply.github.com>
2025-07-29 23:01:11 +0530

    Update Assignment_No_III.cpp

--- Assignment_No_III.cpp
+++ Assignment_No_III.cpp
@@ -1,3 +1,9 @@
+/*Implement a problem of smallest number with at least no trailing 
+zeroes in factorial. 
+Statement: Given a number n. The task is to find the smallest number 
+whose factorial contains at least n trailing zeroes.*/
+
+
 #include<iostream>
 using namespace std;
 
