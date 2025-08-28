b58c5b2927f0e6bb89ff44d59ec88a0ea955c56d
Bharat kolhe <163564571+Bharat-kolhe-3001@users.noreply.github.com>
2025-08-28 20:52:48 +0530

    Assignment_No_VII.cpp

---
+++ Assignment_No_VII.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int total_Days;
+    int Total_Hourse;
+  cout<<"Enter No Of Days";
+  cin>>total_Days;
+
+  cout<<"Enter No Of Tsak For  Hourse Per Index   :-";
+  cin>>Total_Hourse;
+   
+  int task[20];
+  int work;
+  int mean;
+  int count=0;
+  cout<<"Enter Task For Each :-";
+  for(int i=0;i<Total_Hourse;i++){
+    while(work!=-1){
+          if(work<=mean){
+             count++;
+          }
+
+          if(mean<work){
+              count++;
+          }
+    }
+  }  
+}
