558608fc1ff2cad690a4a8ddde2ad06a92d40062
Bharat kolhe <163564571+Bharat-kolhe-3001@users.noreply.github.com>
2025-09-02 21:48:58 +0530

    Assignment_No_VII.cpp

--- Assignment_No_VII.cpp
+++ Assignment_No_VII.cpp
@@ -1,30 +1,18 @@
-
 #include<iostream>
 using namespace std;
 int main(){
-    int total_Days;
-    int Total_Hourse;
-  cout<<"Enter No Of Days";
-  cin>>total_Days;
+    int task[20];
+    int total;
+    cout<<"Enter Task  Length";
+    cin>>total;
 
-  cout<<"Enter No Of Tsak For  Hourse Per Index   :-";
-  cin>>Total_Hourse;
-   
-  int task[20];
-  int work;
-  int mean;
-  int count=0;
-  cout<<"Enter Task For Each :-";
-  for(int i=0;i<Total_Hourse;i++){
-    while(work!=-1){
-          if(work<=mean){
-             count++;
-          }
-
-          if(mean<work){
-              count++;
-          }
+    for(int i=0;i<total;i++){
+        cout<<"Enter "<<i<<" Task:- ";
+        cin>>task[i];
     }
-  }  
-}
+    int deadline=0;
+    cout<<"Enter Days Within The Tsk Should Completed";
+    cin>>deadline;
 
+    
+}
