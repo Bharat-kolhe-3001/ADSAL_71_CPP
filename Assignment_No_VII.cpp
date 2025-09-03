33d9665239a33e7efe09248923ca6e3f4719ad12
Bharat kolhe <163564571+Bharat-kolhe-3001@users.noreply.github.com>
2025-09-03 09:45:03 +0530

    Assignment_No_VII.cpp

--- Assignment_No_VII.cpp
+++ Assignment_No_VII.cpp
@@ -3,16 +3,26 @@ using namespace std;
 int main(){
     int task[20];
     int total;
-    cout<<"Enter Task  Length";
+    cout<<"Enter Task Length: ";
     cin>>total;
 
     for(int i=0;i<total;i++){
-        cout<<"Enter "<<i<<" Task:- ";
+        cout<<"Enter "<<i+1<<" Task:- ";
         cin>>task[i];
     }
+
     int deadline=0;
-    cout<<"Enter Days Within The Tsk Should Completed";
+    cout<<"Enter Days Within The Task Should Be Completed: ";
     cin>>deadline;
 
-    
+    int sum=0;
+    for(int i=0;i<total;i++){
+        sum+=task[i];
+    }
+
+    int hoursPerDay = (sum + deadline - 1) / deadline;
+
+    cout<<"In Every Day You need to work for "<<hoursPerDay<<" Hours";
+
+    return 0;
 }
