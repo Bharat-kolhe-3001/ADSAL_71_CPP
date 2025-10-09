d49b346caf51452d36f74884fb6ae26d29985b8b
Bharat kolhe <163564571+Bharat-kolhe-3001@users.noreply.github.com>
2025-10-09 08:41:43 +0530

    Add files via upload

---
+++ Assignment_No_10.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[100];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int sum;
+    cout << "Enter sum to check: ";
+    cin >> sum;
+
+     bool dp[101][101] = {false};
+
+     for (int i = 0; i <= n; i++)
+        dp[i][0] = true;
+
+ 
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= sum; j++) {
+            if (arr[i - 1] > j)   
+                dp[i][j] = dp[i - 1][j];
+            else
+                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - arr[i - 1]];
+        }
+    }
+
+     
+    if (dp[n][sum])
+        cout << "Subset with given sum exists.";
+    else
+        cout << "No subset with given sum exists.";
+
+    return 0;
+}
